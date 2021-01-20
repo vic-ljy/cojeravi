e4648dbf506bfbc9cef947e6dc3b021ddd5f386e
vic-ljy <munchkinvl@gmail.com>
2021-01-19 19:55:25 -0500

    Final as of end of hackathon
    Tuesday Jan 19 - final product as completed at end of hackathon. Good job guys :)

--- public/js/scheduleFormatting.js
+++ public/js/scheduleFormatting.js
@@ -25,7 +25,9 @@ firebase.auth().onAuthStateChanged((user) => {
             document.getElementById("gc").innerHTML = roomID.toUpperCase();
         });
         //code here runs before code in .once()
-    } 
+    } else {
+        alert("not signed in");
+    }
 })
 
 function twoDigits(input) {
